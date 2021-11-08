ad507be952c3cfff4e5a343ef0b38f894b582927
Chris DePastene <cdepastene@gmail.com>
2021-11-07 21:37:42 -0800

    set array

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  const flavors = ["Vanilla" , "Chocolate" , "Strawberry" , "Mint Chocolate Chip" , "Rocky Road" , "S'mores"]
+
+  $("#reveal").submit(function() {
+    flavors.forEach(function(flavor) { 
+  });
+});
